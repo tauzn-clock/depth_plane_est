800b1e2a785307289638c08807e22881eb241777
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-06-09 03:32:27 +0000

    use metric3d

--- python/get_planes.py
+++ python/get_planes.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-DATA_DIR = "/scratchdata/processed/alcove2"
+DATA_DIR = "/scratchdata/processed/outdoor_highres"
 
 with open(os.path.join(DATA_DIR, "camera_info.json"), "r") as f:
     data = json.load(f)
@@ -12,7 +12,7 @@ with open(os.path.join(DATA_DIR, "camera_info.json"), "r") as f:
     
 print("INTRINSICS:", INTRINSICS)
 
-INDEX = 0
+INDEX = 349
 
 rgb = Image.open(os.path.join(DATA_DIR, "rgb", f"{INDEX}.png")).convert('RGB')
 depth = Image.open(os.path.join(DATA_DIR, "depth", f"{INDEX}.png"))
@@ -21,18 +21,22 @@ rgb = np.array(rgb)
 depth = np.array(depth) / 1000.0  # Convert to meters and scale
 depth = depth #/ 10.0 #depth.max()
 
-"""
-from run_metric3d import run_metric3d
-metric3d_depth, metric3d_normal = run_metric3d(rgb)
-metric3d_normal = metric3d_normal * np.where(metric3d_normal[:,:,2]> 0, 1, -1)[..., np.newaxis]
-
-plt.imsave("metric3d_depth.png", metric3d_depth)
-plt.imsave("metric3d_normal.png", (metric3d_normal + 1) / 2.0)
-"""
 from depth_plane_est.process_depth import get_normal_adj, get_normal_svd
 normal = get_normal_svd(depth, INTRINSICS, kernel_size=21)
 
 plt.imsave("normal.png", (normal + 1) / 2.0)
+
+if True:
+    from run_metric3d import run_metric3d
+    metric3d_depth, metric3d_normal = run_metric3d(rgb)
+    print(metric3d_depth.max(), metric3d_depth.min())
+    metric3d_normal = metric3d_normal * np.where(metric3d_normal[:,:,2]> 0, 1, -1)[..., np.newaxis]
+
+    plt.imsave("metric3d_depth.png", metric3d_depth)
+    plt.imsave("metric3d_normal.png", (metric3d_normal + 1) / 2.0)
+
+    depth = metric3d_depth
+    normal = metric3d_normal
 
 from depth_plane_est.get_planes import get_planes
 mask, param = get_planes(depth, normal, INTRINSICS, 5, 0.02)
