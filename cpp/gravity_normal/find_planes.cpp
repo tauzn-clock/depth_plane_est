e22d87a429ab06668db08b4d610cb35c6b298249
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-06-27 06:21:08 +0000

    plane output as pcd

--- cpp/gravity_normal/find_planes.cpp
+++ cpp/gravity_normal/find_planes.cpp
@@ -69,16 +69,43 @@ void depthImageCallback(const sensor_msgs::Image::ConstPtr& msg){
     std::vector< std::array<float, 3> > img_normals = get_normal(points);
     centre_hemisphere(img_normals,gravity_vector);
 
-    save_normal(img_normals, W, H, "/catkin_ws/src/depth_plane_est/normal.png");
-    ROS_INFO("Normal image saved to /catkin_ws/src/depth_plane_est/normal.png");
+    //save_normal(img_normals, W, H, "/catkin_ws/src/depth_plane_est/normal.png");
+    //ROS_INFO("Normal image saved to /catkin_ws/src/depth_plane_est/normal.png");
 
     correct_vector(img_normals, gravity_vector, config["dot_bound"].as<float>(), config["correction_iteration"].as<int>());
     centre_hemisphere(img_normals,gravity_vector);
 
     std::vector<int> mask = find_peaks(img_normals, points, gravity_vector, config["dot_bound"].as<float>(), config["kernel_size"].as<int>(), config["cluster_size"].as<int>());
 
-    save_mask(mask, W, H, "/catkin_ws/src/depth_plane_est/mask.png");
-    ROS_INFO("Mask saved to /catkin_ws/src/depth_plane_est/mask.png");
+    //save_mask(mask, W, H, "/catkin_ws/src/depth_plane_est/mask.png");
+    //ROS_INFO("Mask saved to /catkin_ws/src/depth_plane_est/mask.png");
+
+    int max_mask = 0;
+    for (int i = 0; i < mask.size(); ++i) {
+        if (mask[i] > max_mask) {
+            max_mask = mask[i];
+        }
+    }
+
+    // Color points based on the mask
+    for (int i = 0; i < points.size(); ++i) {
+        if (mask[i] == 0) {
+            points[i].r = 0;
+            points[i].g = 0;
+            points[i].b = 0; // Black for background
+            continue;
+        }
+        std::array<int,3> color = hsv(mask[i]-1, max_mask);
+        points[i].r = color[0];
+        points[i].g = color[1];
+        points[i].b = color[2];
+    }
+
+    sensor_msgs::PointCloud2 cloud_msg;
+    pcl::toROSMsg(points, cloud_msg);
+    cloud_msg.header.frame_id = "camera_link";
+    cloud_msg.header.stamp = ros::Time::now();
+    cloud_pub.publish(cloud_msg);
 }
 
 int main(int argc, char** argv)
@@ -103,7 +130,7 @@ int main(int argc, char** argv)
     ros::Subscriber depth_img_sub = nh.subscribe(depth_img_topic, 24, depthImageCallback);
     ros::Subscriber camera_info_sub = nh.subscribe(depth_intrinsic_topic, 1, depthIntrinsicCallback);
 
-    cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/grav_normals", 1);
+    cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/grav_normals_pcd", 1);
 
     // Spin to keep the node alive
     ros::spin();
