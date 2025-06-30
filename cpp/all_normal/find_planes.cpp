31a820d01f9a3675b3601008c1133614cf110303
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-06-30 07:56:44 +0000

    update main script for all plane

--- cpp/all_normal/find_planes.cpp
+++ cpp/all_normal/find_planes.cpp
@@ -18,27 +18,10 @@
 #define VISUALISE true
 
 YAML::Node config;
-sensor_msgs::Imu imu;
 sensor_msgs::CameraInfo camera_info;
 ros::Publisher cloud_pub;
 
-bool imuReceived = false;
 bool cameraInfoReceived = false;
-
-void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
-    imu.angular_velocity.x = msg->angular_velocity.x;
-    imu.angular_velocity.y = msg->angular_velocity.y;
-    imu.angular_velocity.z = msg->angular_velocity.z;
-    imu.linear_acceleration.x = msg->linear_acceleration.x;
-    imu.linear_acceleration.y = msg->linear_acceleration.y;
-    imu.linear_acceleration.z = msg->linear_acceleration.z;
-    imu.orientation.x = msg->orientation.x;
-    imu.orientation.y = msg->orientation.y;
-    imu.orientation.z = msg->orientation.z;
-    imu.orientation.w = msg->orientation.w;
-
-    imuReceived = true;
-}
 
 void depthIntrinsicCallback(const sensor_msgs::CameraInfo::ConstPtr& msg){
     camera_info.header = msg->header;
@@ -55,8 +38,8 @@ void depthIntrinsicCallback(const sensor_msgs::CameraInfo::ConstPtr& msg){
 
 void depthImageCallback(const sensor_msgs::Image::ConstPtr& msg){
 
-    if (!imuReceived || !cameraInfoReceived) {
-        ROS_WARN("IMU or Camera Info not received yet.");
+    if (!cameraInfoReceived) {
+        ROS_WARN("Camera Info not received yet.");
         return;
     }
 
@@ -68,9 +51,11 @@ void depthImageCallback(const sensor_msgs::Image::ConstPtr& msg){
     std::vector< std::array<float, 3> > img_normals = get_normal(points);
 
     std::array< float, 3> z_axis = {0.0f, 0.0f, 1.0f};
-    centre_hemisphere(img_normals,z_axis);
+    centre_hemisphere(img_normals, z_axis);
 
     cluster_normal(img_normals, config["angle_bins"].as<int>());
+
+    ROS_INFO("%f", M_PI);
 }
 
 int main(int argc, char** argv)
@@ -81,21 +66,19 @@ int main(int argc, char** argv)
     // Create a node handle
     ros::NodeHandle nh;
 
-    std::string imu_topic, depth_img_topic, depth_intrinsic_topic;
-    nh.getParam("gravity_normal/imu_topic", imu_topic);
-    nh.getParam("gravity_normal/depth_img_topic", depth_img_topic);
-    nh.getParam("gravity_normal/depth_intrinsic_topic", depth_intrinsic_topic);
+    std::string depth_img_topic, depth_intrinsic_topic;
+    nh.getParam("all_normal/depth_img_topic", depth_img_topic);
+    nh.getParam("all_normal/depth_intrinsic_topic", depth_intrinsic_topic);
 
     std::string yaml_file_path;
-    nh.getParam("gravity_normal/yaml_file_path", yaml_file_path);
+    nh.getParam("all_normal/yaml_file_path", yaml_file_path);
     std::cout << "YAML file path: " << yaml_file_path << std::endl;
     config = YAML::LoadFile(yaml_file_path);
 
-    ros::Subscriber imu_sub = nh.subscribe(imu_topic, 100, imuCallback);
     ros::Subscriber depth_img_sub = nh.subscribe(depth_img_topic, 24, depthImageCallback);
     ros::Subscriber camera_info_sub = nh.subscribe(depth_intrinsic_topic, 1, depthIntrinsicCallback);
 
-    cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/grav_normals_pcd", 1);
+    cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/all_normals_pcd", 1);
 
     // Spin to keep the node alive
     ros::spin();
